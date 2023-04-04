ec332d32205074b44868ca6ca993092341c5df17
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2023-04-04 17:11:50 +1000

    PC_Work commit
    working with formation script

--- Assets/Model/Formation.cs
+++ Assets/Model/Formation.cs
@@ -15,10 +15,8 @@ public class Formation : MonoBehaviour
         Pawn
     }
 
-    private int[] _playerFiguresFormationKeys;
-    private int[] _enemyFiguresFormationKeys;
-
-    private Dictionary<Figure, Vector3> _playerFiguresPoints;
+    private int[] _figuresFormationKeys;
+    private Dictionary<String, Vector3> _playerFiguresPoints;
     private Dictionary<Figure, Vector3> _enemyFiguresPoints;
 
     public int MaxFiguresRows { get; private set; }
@@ -26,19 +24,24 @@ public class Formation : MonoBehaviour
 
     private void Awake()
     {
-        _playerFiguresFormationKeys = new int[] { 0, 1, 2, 3, 4, 2, 1, 0, 5, 5, 5, 5, 5, 5, 5, 5 };
-        _enemyFiguresFormationKeys = new int[] { 0, 1, 2, 3, 4, 2, 1, 0, 5, 5, 5, 5, 5, 5, 5, 5 };
+        _figuresFormationKeys = new int[] { 0, 1, 2, 3, 4, 2, 1, 0, 5, 5, 5, 5, 5, 5, 5, 5 };
         MaxFiguresRows = 2;
         MaxFiguresColumns = 8;
-        _playerFiguresPoints = new Dictionary<Figure, Vector3>();
+        _playerFiguresPoints = new Dictionary<String, Vector3>();
         _enemyFiguresPoints = new Dictionary<Figure, Vector3>();
+
+        CreatePlayerFiguresPoints(new Vector3(0,0));
     }
 
     public void CreatePlayerFiguresPoints(Vector3 startPosition)
     {
         _playerFiguresPoints.Clear();
-        _playerFiguresPoints.Add(0, startPosition);
 
+        for (int i = 0; i < _figuresFormationKeys.Length; i++)
+        {
+            _playerFiguresPoints.Add(Enum.GetName(typeof(Figure), _figuresFormationKeys[i]), new Vector3(startPosition.x + i, startPosition.y));
+            Debug.Log(_playerFiguresPoints[Enum.GetName(typeof(Figure), _figuresFormationKeys[i])]);
+        }
     }
 
     public void CreateEnemyFiguresPoints(Vector3 startPosition)

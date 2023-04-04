0dc222df5dc3b5299d6d0c64278adc67a893d6d0
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2023-04-04 10:51:39 +1000

    PC_Work commit
    add spawner and item scripts

---
+++ Assets/Model/Item.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private string _name;
+    private string _description;
+    private int _count;
+}
--- Assets/Model/Player.cs
+++ Assets/Model/Player.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private List<Item> _inventory;
+    private string _name;
+    private int _level;
+    private int _experience;
+
     public int FiguresCount { get; private set; }
+
+    public void ChangeName(string name)
+    {
+        _name = name;
+    }
+    
+    public void ChangeExperience(int experience)
+    {
+        _experience += experience;
+    }
+
+    public void IncreaseLevel()
+    {
+        _level++;
+    }
 }

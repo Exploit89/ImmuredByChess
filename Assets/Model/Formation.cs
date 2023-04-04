8bd23a613213118c83103116d7270ef3707b1c33
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2023-04-04 14:25:19 +1000

    PC_Work commit
    still working with formation script

--- Assets/Model/Formation.cs
+++ Assets/Model/Formation.cs
@@ -15,7 +15,8 @@ public class Formation : MonoBehaviour
         Pawn
     }
 
-    private int[] _figuresFormationKeys;
+    private int[] _playerFiguresFormationKeys;
+    private int[] _enemyFiguresFormationKeys;
 
     private Dictionary<Figure, Vector3> _playerFiguresPoints;
     private Dictionary<Figure, Vector3> _enemyFiguresPoints;
@@ -25,6 +26,8 @@ public class Formation : MonoBehaviour
 
     private void Awake()
     {
+        _playerFiguresFormationKeys = new int[] { 0, 1, 2, 3, 4, 2, 1, 0, 5, 5, 5, 5, 5, 5, 5, 5 };
+        _enemyFiguresFormationKeys = new int[] { 0, 1, 2, 3, 4, 2, 1, 0, 5, 5, 5, 5, 5, 5, 5, 5 };
         MaxFiguresRows = 2;
         MaxFiguresColumns = 8;
         _playerFiguresPoints = new Dictionary<Figure, Vector3>();

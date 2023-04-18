1c71f5e74cd9607bde596711e81f81f9a9f883d0
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2023-04-18 16:38:24 +1000

    PC_Work commit

--- Assets/Model/Pieces/Piece.cs
+++ Assets/Model/Pieces/Piece.cs
@@ -16,13 +16,12 @@ public abstract class Piece : MonoBehaviour
     [SerializeField] protected PieceType _type;
 
     protected int _maxMovementLenght = 8;
-
-    public PieceType Type => _type;
-
     protected Vector2Int[] RookDirections = {new Vector2Int(0,1), new Vector2Int(1, 0),
         new Vector2Int(0, -1), new Vector2Int(-1, 0)};
     protected Vector2Int[] BishopDirections = {new Vector2Int(1,1), new Vector2Int(1, -1),
         new Vector2Int(-1, -1), new Vector2Int(-1, 1)};
+
+    public PieceType Type => _type;
 
     public abstract List<Vector2Int> MoveLocations(Vector2Int gridPoint);
 }

be3d2a74993b08ef80465d4ac581dd61d3571c7f
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2023-04-30 17:44:14 +1000

    PC_Home commit
    complete start camera mover script

--- Assets/Scripts/Model/CameraMover.cs
+++ Assets/Scripts/Model/CameraMover.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 
@@ -7,32 +8,53 @@ public class CameraMover : MonoBehaviour
 
     private Vector3 _startPosition = new Vector3(3, 50, -4.5f);
     private Vector3 _destination = new Vector3(3, 7.5f, -4.5f);
-    private Vector3 _rotation = new Vector3(50, 0, 0);
-    private float _speed = 8f;
+    private Quaternion _startRotation;
+    private Quaternion _destinationRotation;
+    private float _speed = 12f;
+    private float _startXAngle = 10;
+    private float _endXAngle = 50;
+    private Coroutine _currentCoroutine;
 
     void Awake()
     {
         transform.position = _startPosition;
+        _startRotation = Quaternion.Euler(_startXAngle, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        _destinationRotation = Quaternion.Euler(_endXAngle, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        transform.rotation = _startRotation;
     }
 
     private void OnEnable()
     {
-        _inputName.NameEntered += StartMove;
+        _inputName.NameEntered += Move;
     }
 
     private void OnDisable()
     {
-        _inputName.NameEntered -= StartMove;
+        _inputName.NameEntered -= Move;
     }
 
-    private void StartMove()
+    public IEnumerator ChangePosition()
     {
-        StartCoroutine("Move");
+        var waitingTime = new WaitForSeconds(0.001f);
+
+        while (transform.position != _destination || transform.rotation != _destinationRotation)
+        {
+            transform.position = Vector3.MoveTowards(_startPosition, _destination, Time.deltaTime * _speed);
+            transform.rotation = Quaternion.RotateTowards(_startRotation, _destinationRotation, Time.deltaTime * _speed);
+            _startPosition = transform.position;
+            _startRotation = transform.rotation;
+            yield return waitingTime;
+        }
     }
 
     public void Move()
     {
-        transform.position = Vector3.MoveTowards(_startPosition, _destination, Time.deltaTime * _speed);
-        _startPosition = transform.position;
+        if (_currentCoroutine != null)
+            StopCoroutine(_currentCoroutine);
+
+        if (transform.position != _destination)
+            _currentCoroutine = StartCoroutine(ChangePosition());
+        else
+            StopCoroutine(_currentCoroutine);
     }
 }

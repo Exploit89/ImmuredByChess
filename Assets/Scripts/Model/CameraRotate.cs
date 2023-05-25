5571b9bdc08ed7daba6d6fb2ae0d36c09b5d56aa
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2023-05-25 14:45:03 +1000

    PC_Work commit
    try to rotate camera

---
+++ Assets/Scripts/Model/CameraRotate.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private void LateUpdate()
+    {
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+
+        }
+    }
+}
